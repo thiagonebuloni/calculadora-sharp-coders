bed99f24da9fea63fb6381dd18448cc8588f11f1
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-02-12 21:25:18 -0300

    add funcionalidade nos botões | add avatar

---
+++ script.js
@@ -0,0 +1,35 @@
+function clean() {
+    var clearing = ' ';
+    var last_op = document.getElementById('last-operation');
+    last_op.innerHTML = clearing;
+    
+    var result = document.getElementById('result-number');
+    result.innerHTML = clearing;
+}
+
+function print(number) {
+    var screen = document.getElementById('last-operation');
+    screen.innerHTML += number;
+}
+
+function two() {
+    var number = '2';
+    var screen = document.getElementById('last-operation');
+    screen.innerHTML += number;
+}
+
+function plus() {
+    var sign = '+';
+    var screen = document.getElementById('last-operation');
+    screen.innerHTML += sign;
+}
+
+function equals() {
+    var last_op = parseInt(document.getElementById("last-operation"));
+    var screen = document.getElementById('result-number');
+    screen.innerHTML = parseInt(last_op);
+}
+
+function home() {
+    window.alert('Thiago Nebuloni ;)');
+}
